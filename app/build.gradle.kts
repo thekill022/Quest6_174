b3ab61dcc2e95d9b35465dc5afdb11b359d972df
thekill022 <gibfathoni@gmail.com>
2025-11-21 09:50:47 +0700

    penyesuaian ulang graddle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-android:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
