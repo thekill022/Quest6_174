b5193dd6ccc25aabca6609f65503e13231cda866
thekill022 <gibfathoni@gmail.com>
2025-11-21 10:22:01 +0700

    membuat fungsi untuk set status siswa apabila ada perubahan di form setelah submit

--- app/src/main/java/com/example/mvvc_consept/viewmodel/siswaViewModel.kt
+++ app/src/main/java/com/example/mvvc_consept/viewmodel/siswaViewModel.kt
@@ -3,9 +3,19 @@ package com.example.mvvc_consept.viewmodel
 import androidx.lifecycle.ViewModel
 import com.example.mvvc_consept.model.siswa
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class siswaViewModel : ViewModel() {
 
     private val _statusUI = MutableStateFlow(siswa())
+    val statusUI : StateFlow<siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls : MutableList<String>) {
+        _statusUI.update { statusNow ->
+            statusNow.copy(nama = ls[0], gender = ls[1], alamat = ls[2])
+        }
+    }
 
 }
