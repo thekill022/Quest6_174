025c3db7717b3a19ad2a0579fb48ada1ad420bca
thekill022 <gibfathoni@gmail.com>
2025-11-21 10:00:49 +0700

    membuat composable function untuk navigasi

--- app/src/main/java/com/example/mvvc_consept/view/navigation.kt
+++ app/src/main/java/com/example/mvvc_consept/view/navigation.kt
@@ -1,8 +1,17 @@
 package com.example.mvvc_consept.view
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
     FormSiswa,
     TampilData
+}
+
+@Composable
+fun DataApp(
+    navController : NavHostController = rememberNavController()
+) {
+
 }
