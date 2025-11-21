b4cc567d3c6d3fb6f4dd4ccb89fb7dcf1570a6cd
thekill022 <gibfathoni@gmail.com>
2025-11-21 10:19:41 +0700

    membuat mutable state flow yang menampung data dari class siswa

--- app/src/main/java/com/example/mvvc_consept/viewmodel/siswaViewModel.kt
+++ app/src/main/java/com/example/mvvc_consept/viewmodel/siswaViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.mvvc_consept.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.example.mvvc_consept.model.siswa
+import kotlinx.coroutines.flow.MutableStateFlow
 
 class siswaViewModel : ViewModel() {
+
+    private val _statusUI = MutableStateFlow(siswa())
 
 }
