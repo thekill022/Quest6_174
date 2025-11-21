e9db59dea0a6738204561f4664487655996be8c7
thekill022 <gibfathoni@gmail.com>
2025-11-21 10:05:40 +0700

    membuat navhost untuk membuangkus masing masing route destination

--- app/src/main/java/com/example/mvvc_consept/view/navigation.kt
+++ app/src/main/java/com/example/mvvc_consept/view/navigation.kt
@@ -1,7 +1,12 @@
 package com.example.mvvc_consept.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
 enum class Navigasi {
@@ -13,5 +18,16 @@ enum class Navigasi {
 fun DataApp(
     navController : NavHostController = rememberNavController()
 ) {
+
+    Scaffold {
+        innerPad ->
+        NavHost(navController = navController,
+            startDestination = Navigasi.FormSiswa.name,
+            modifier = Modifier.padding(innerPad)) {
+            composable(route = Navigasi.FormSiswa.name) {
+                TampilData(onBackBtnClick = )
+            }
+        }
+    }
 
 }
